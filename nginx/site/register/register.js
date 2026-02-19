f454f78775674676c3dd2f6f6b4e7b18a28da491
Gabriel Eudes <gaeudes@student.42.fr>
2026-02-19 14:21:25 +0100

    Hacker de rang S

--- nginx/site/register/register.js
+++ nginx/site/register/register.js
@@ -1,7 +1,29 @@
 function press() {
 	let username = document.getElementById("username").value
+	let email = document.getElementById("email").value
 	let password1 = document.getElementById("password_1").value
 	let password2 = document.getElementById("password_2").value
-	console.log(username)
+	if (password1 === password2) {
+		console.log("Password match")
+		const response = fetch('https://localhost:1043/api/register', {
+			mode: 'no-cors',
+			method: 'POST',
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				'username': username,
+				'Email': email,
+				'Password': password1,
+			})
+		})
+		if (!response.Ok) {
+			console.log("No ok")
+			return
+		}
+	}
+	console.log(username, password1, password2)
 	return username
 }
