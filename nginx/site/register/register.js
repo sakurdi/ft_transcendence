dbc559de9aa825aea5f2ba89412990da21ffc3c7
Gabriel Eudes <gaeudes@student.42.fr>
2026-02-19 12:36:48 +0100

    Added temp favicon

---
+++ nginx/site/register/register.js
@@ -0,0 +1,7 @@
+function press() {
+	let username = document.getElementById("username").value
+	let password1 = document.getElementById("password_1").value
+	let password2 = document.getElementById("password_2").value
+	console.log(username)
+	return username
+}
